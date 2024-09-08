71ed09a884acadb96eb4348735550e1394449086
mennaelgenbehy143 <mennaelgenbehy@gmail.com>
2024-09-08 19:19:42 +0300

    OnBoarding Screen

---
+++ lib/features/onBoarding/widgets/get_started_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/core/helpers/extentions.dart';
+import 'package:flutter_project/core/theming/colors.dart';
+import 'package:flutter_project/core/theming/styles.dart';
+
+import '../../../core/routing/routes.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (){
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(ColorsManger.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(
+          Size(double.infinity, 52),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          )
+        )
+      ),
+      child: Text(
+          'Get Started',
+         style: TextStyles.font16WhiteSemiBold,
+      ),
+    );
+  }
+}
